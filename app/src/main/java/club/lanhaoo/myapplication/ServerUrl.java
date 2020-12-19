be8f6f757ba0a7faa9a939fd349b19852324fdca
LanHao <a913538526@gmail.com>
2020-12-19 21:16:30 +0800

    加載數據庫内容

---
+++ app/src/main/java/club/lanhaoo/myapplication/ServerUrl.java
@@ -0,0 +1,12 @@
+package club.lanhaoo.myapplication;
+
+public class ServerUrl {
+    public String serverUrl = "http://192.168.1.106/movie/";
+    public String php = ".php";
+
+    public String getServerUrl(String api) {
+        return serverUrl + api + php;
+    }
+
+
+}
---
+++ app/src/main/java/club/lanhaoo/myapplication/VolleyCallBack.java
@@ -0,0 +1,9 @@
+package club.lanhaoo.myapplication;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public interface VolleyCallBack {
+    void onSuccess();
+}
