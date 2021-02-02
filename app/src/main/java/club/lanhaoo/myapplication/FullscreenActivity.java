735110ca342653e8a62c1a1bcdc570825e27c6d7
LanHao <a913538526@gmail.com>
2021-02-02 18:57:35 +0800

    增加快进快退

--- app/src/main/java/club/lanhaoo/myapplication/FullscreenActivity.java
+++ app/src/main/java/club/lanhaoo/myapplication/FullscreenActivity.java
@@ -3,12 +3,12 @@ package club.lanhaoo.myapplication;
 import android.annotation.SuppressLint;
 
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -21,6 +21,8 @@ import com.devbrackets.android.exomedia.ui.widget.VideoView;
  */
 public class FullscreenActivity extends Activity {
     private VideoView videoView;
+    private boolean pause = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +33,7 @@ public class FullscreenActivity extends Activity {
                 (String) intent.getSerializableExtra(DetailsActivity.MOVIE);
 
 
-        videoView = (VideoView)findViewById(R.id.video_view);
+        videoView = (VideoView) findViewById(R.id.video_view);
         videoView.setOnPreparedListener(new OnPreparedListener() {
             @Override
             public void onPrepared() {
@@ -41,7 +43,40 @@ public class FullscreenActivity extends Activity {
         });
 
         videoView.setVideoURI(Uri.parse(videoUrl));
+        videoView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                System.out.println("keyCode: " + keyCode);
+                VideoView view = (VideoView) v;
+                if (keyCode == 23) {
+                    //center
+                    if (!pause) {
+                        view.pause();
+                        pause = true;
+                    } else {
+                        view.start();
+                        pause = false;
+                    }
+                } else if (keyCode == 22) {
+                    //right
+                    if (view.getDuration() > view.getCurrentPosition() + 60000) {
+                        view.seekTo(view.getCurrentPosition() + 60000);
+                    } else {
+                        view.seekTo(view.getDuration() - 30000);
+                    }
 
+                } else if (keyCode == 21) {
+                    //left
+                    if (view.getCurrentPosition() - 60000 > 0) {
+                        view.seekTo(view.getCurrentPosition() - 60000);
+                    } else {
+                        view.seekTo(0);
+                    }
+                }
+                System.out.println("event: " + event.toString());
+                return false;
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/club/lanhaoo/myapplication/ws/ChatServer.java
@@ -0,0 +1,90 @@
+package club.lanhaoo.myapplication.ws;
+
+import android.content.Context;
+import android.content.Intent;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Collections;
+
+import club.lanhaoo.myapplication.DetailsActivity;
+import club.lanhaoo.myapplication.FullscreenActivity;
+
+public class ChatServer extends WebSocketServer {
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    private Context context;
+
+    public ChatServer(int port) throws UnknownHostException {
+        super(new InetSocketAddress(port));
+    }
+
+    public ChatServer(InetSocketAddress address) {
+        super(address);
+    }
+
+    public ChatServer(int port, Draft_6455 draft) {
+        super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        conn.send("Welcome to the server!"); //This method sends a message to the new client
+        broadcast("new connection: " + handshake
+                .getResourceDescriptor()); //This method sends a message to all clients connected
+        System.out.println(
+                conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!");
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        broadcast(conn + " has left the room!");
+        System.out.println(conn + " has left the room!");
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        broadcast(message);
+        System.out.println(conn + ": " + message);
+        if (message.contains(".m3u8")){
+            Intent intent=new Intent(context, FullscreenActivity.class);
+            intent.putExtra(DetailsActivity.MOVIE, message);
+            context.startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, ByteBuffer message) {
+        broadcast(message.array());
+        System.out.println(conn + ": " + message);
+    }
+
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        ex.printStackTrace();
+        if (conn != null) {
+            // some errors like port binding failed may not be assignable to a specific websocket
+        }
+    }
+
+    @Override
+    public void onStart() {
+        System.out.println("Server started!");
+        setConnectionLostTimeout(0);
+        setConnectionLostTimeout(100);
+    }
+
+}
